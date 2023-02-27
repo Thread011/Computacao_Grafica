17d222c39fbca36499aef769473351a68f1d10a3
Thread011 <al73550@utad.eu>
2023-02-27 15:08:19 +0000

    added files

---
+++ Tutorial_1/Javascript/app.js
@@ -0,0 +1,113 @@
+var canvas = document.createElement('canvas');
+
+canvas.height = window.innerHeight - 100;
+canvas.width = window.innerWidth - 15;
+
+var GL = canvas.getContext('webgl');
+
+var vertexShader = GL.createShader(GL.VERTEX_SHADER);
+
+var fragmentShader = GL.createShader(GL.FRAGMENT_SHADER);
+
+var program = GL.createProgram();
+
+var gpuArrayBuffer = GL.createBuffer();
+
+function PrepareCanvas(){
+    GL.clearColor(0.65, 0.65, 0.65, 1.0);
+
+    GL.clear(GL.DEPTH_BUFFER_BIT | GL.COLOR_BUFFER_BIT);
+
+    document.body.appendChild(canvas);
+
+    canvas.insertAdjacentText('afterend', 'O canvas encontra-se acima deste texto!');
+}
+
+function PrepareShaders(){
+    GL.shaderSource(vertexShader, codigoVertexShader);
+    GL.shaderSource(fragmentShader, codigoFragmentShader);
+
+
+    GL.compileShader(vertexShader);
+    GL.compileShader(fragmentShader);
+
+    if(!GL.getShaderParameter(vertexShader, GL.COMPILE_STATUS)){
+        console.error("ERRO :: A compilação do vertex shader lançou uma excepção.",
+            GL.getShaderInfoLog(vertexShader));
+    }
+
+
+    if(!GL.getShaderParameter(fragmentShader, GL.COMPILE_STATUS)){
+        console.error("ERRO :: A compilação do fragment shader lançou uma excepção.",
+            GL.getShaderInfoLog(fragmentShader));
+    }
+
+}
+
+function PrepareProgram(){
+    GL.attachShader(program, vertexShader);
+    GL.attachShader(program, fragmentShader);
+
+    GL.linkProgram(program);
+    if(!GL.getProgramParameter(program, GL.LINK_STATUS)){
+        console.error("ERRO :: O linkProgram lançou uma excepção", GL.getProgramInfoLog(program));
+    }
+
+
+    GL.validateProgram(program);
+    if(!GL.getProgramParameter(program, GL.VALIDATE_STATUS)){
+        console.error("ERRO :: A validação do program lançou uma excepção", GL.getProgramInfoLog(program));
+    }
+
+    GL.useProgram(program);
+}
+
+function PrepareTriangleData(){
+    var triangleArray = [
+        -0.5,   -0.5,   0.0,    1.0,    0.0,    0.0,
+        0.5,    -0.5,   0.0,    0.0,    1.0,    0.0,
+        0.0,    0.5,    0.0,    0.0,    0.0,    1.0
+    ];
+
+    GL.bindBuffer(GL.ARRAY_BUFFER, gpuArrayBuffer);
+
+    GL.bufferData(
+        GL.ARRAY_BUFFER,
+        new Float32Array(triangleArray),
+        GL.STATIC_DRAW
+    );
+}
+
+function SendDataToShaders(){
+    var vertexPositionAttributeLocation = GL.getAttribLocation(program, "vertexPosition");
+    var vertexColorAttributeLocation = GL.getAttribLocation(program, "vertexColor");
+
+    GL.vertexAttribPointer(
+        vertexPositionAttributeLocation,
+        3,
+        GL.FLOAT,
+        false,
+        6*Float32Array.BYTES_PER_ELEMENT,
+        0*Float32Array.BYTES_PER_ELEMENT
+    );
+
+    GL.enableVertexAttribArray(vertexPositionAttributeLocation);
+    GL.enableVertexAttribArray(vertexColorAttributeLocation);
+
+    GL.useProgram(program);
+
+    GL.drawArrays(
+        GL.TRIANGLES,
+        0,
+        3
+    );
+
+}
+
+function Start(){
+    PrepareCanvas();
+    PrepareShaders();
+    PrepareProgram();
+    PrepareTriangleData();
+    SendDataToShaders();
+}
