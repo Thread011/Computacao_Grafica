73829c31295db3ebe94411ddaab76e32f9e64681
Thread011 <al73550@utad.eu>
2023-03-16 16:13:24 +0000

    tuto_2

---
+++ Tutorial_2/Javascript/app.js
@@ -0,0 +1,185 @@
+
+var canvas = document.createElement('canvas');
+
+//Dimension of the canvas element.
+canvas.width = window.innerWidth - 15;
+canvas.height = window.innerHeight - 100;
+
+
+//Create a WebGLRenderingContext.
+var GL = canvas.getContext('webgl');
+
+//Declare vertex & fragment shader types:
+var vertexShader = GL.createShader(GL.VERTEX_SHADER);
+var fragmentShader = GL.createShader(GL.FRAGMENT_SHADER);
+
+//Declare the program variable:
+var program = GL.createProgram();
+
+var gpuArrayBuffer = GL.createBuffer();
+
+var finalMatrixLocation;
+
+var anguloDeRotacao = 0;
+
+function PrepareCanvas(){
+    GL.clearColor(0.65, 0.65, 0.65, 1.0);
+
+    GL.clear(GL.DEPTH_BUFFER_BIT | GL.COLOR_BUFFER_BIT);
+
+    document.body.appendChild(canvas);
+
+    canvas.insertAdjacentText('afterend', 'O canvas encontra-se acima deste texto!');
+}
+
+
+//Creates shaders, by adding its source, compiling it, and check if compilation is successful
+function PrepareShaders(){
+    GL.shaderSource(vertexShader, codigoVertexShader);
+    GL.shaderSource(fragmentShader, codigoFragmentShader);
+
+
+    GL.compileShader(vertexShader);
+    GL.compileShader(fragmentShader);
+
+    if(!GL.getShaderParameter(vertexShader, GL.COMPILE_STATUS)){
+        console.error("ERRO :: A compilação do vertex shader lançou uma excepção.",
+            GL.getShaderInfoLog(vertexShader));
+    }
+
+
+    if(!GL.getShaderParameter(fragmentShader, GL.COMPILE_STATUS)){
+        console.error("ERRO :: A compilação do fragment shader lançou uma excepção.",
+            GL.getShaderInfoLog(fragmentShader));
+    }
+
+}
+
+//Creates the program, composed by vertex & fragment shaders.
+
+function PrepareProgram(){
+    GL.attachShader(program, vertexShader);
+    GL.attachShader(program, fragmentShader);
+
+    GL.linkProgram(program);
+    if(!GL.getProgramParameter(program, GL.LINK_STATUS)){
+        console.error("ERRO :: O linkProgram lançou uma excepção", GL.getProgramInfoLog(program));
+    }
+
+
+    GL.validateProgram(program);
+    if(!GL.getProgramParameter(program, GL.VALIDATE_STATUS)){
+        console.error("ERRO :: A validação do program lançou uma excepção", GL.getProgramInfoLog(program));
+    }
+
+    GL.useProgram(program);
+}
+
+//Float array representing the points to be drawn (x,y,z) + colors R, G, B
+function PrepareTriangleData(){
+    var triangleArray = [
+        -0.5,   -0.5,   0.0,    1.0,    0.0,    0.0,
+        0.5,    -0.5,   0.0,    0.0,    1.0,    0.0,
+        0.0,    0.5,    0.0,    0.0,    0.0,    1.0
+    ];
+
+    GL.bindBuffer(GL.ARRAY_BUFFER, gpuArrayBuffer);
+
+    GL.bufferData(
+        GL.ARRAY_BUFFER,
+        new Float32Array(triangleArray),
+        GL.STATIC_DRAW
+    );
+}
+
+function SendDataToShaders(){
+    var vertexPositionAttributeLocation = GL.getAttribLocation(program, "vertexPosition");
+    var vertexColorAttributeLocation = GL.getAttribLocation(program, "vertexColor");
+
+    GL.vertexAttribPointer(
+        vertexPositionAttributeLocation,
+        3,
+        GL.FLOAT,
+        false,
+        6*Float32Array.BYTES_PER_ELEMENT,
+        0*Float32Array.BYTES_PER_ELEMENT
+    );
+
+    GL.vertexAttribPointer(
+        vertexColorAttributeLocation,
+        3,
+        GL.FLOAT,
+        false,
+        6*Float32Array.BYTES_PER_ELEMENT,
+        3*Float32Array.BYTES_PER_ELEMENT
+    );
+
+    GL.enableVertexAttribArray(vertexPositionAttributeLocation);
+    GL.enableVertexAttribArray(vertexColorAttributeLocation);
+
+    finalMatrixLocation = GL.getUniformLocation(program, 'transformationMatrix');
+
+}
+
+function loop(){
+    canvas.width = window.innerWidth - 15;
+    canvas.height = window.innerHeight - 100;
+    GL.viewport(0,0, canvas.width, canvas.height);
+
+    GL.useProgram(program);
+
+    GL.clearColor(0.65, 0.65, 0.65, 1.0);
+    GL.clear(GL.DEPTH_BUFFER_BIT | GL.COLOR_BUFFER_BIT);
+
+    var finalMatrix = [
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,1]
+    ];
+
+
+    //DESAFIOS:
+        //Escala: 75% do tamanho original;
+        //Translação: 
+        //Rotação:
+
+    finalMatrix = math.multiply(CriarMatrizEscala(0.25, 0.25, 0.25), finalMatrix);
+    finalMatrix = math.multiply(CriarMatrizEscala(3, 3, 3), finalMatrix);
+
+
+
+    finalMatrix = math.multiply(CriarMatrizRotacaoY(anguloDeRotacao), finalMatrix);
+    finalMatrix = math.multiply(CriarMatrizRotacaoZ(anguloDeRotacao), finalMatrix);
+
+
+    finalMatrix = math.multiply(CriarMatrizTranslacao(0.5, 0.5, 0), finalMatrix);
+    finalMatrix = math.multiply(CriarMatrizTranslacao(-1, -0.9, 0), finalMatrix);
+
+    var newarray = [];
+    for(i = 0; i < finalMatrix.length; i++){
+        newarray = newarray.concat(finalMatrix[i]);
+    }
+
+    GL.uniformMatrix4fv(finalMatrixLocation, false, newarray);
+
+    GL.drawArrays(
+        GL.TRIANGLES,
+        0,
+        3
+    );
+
+    anguloDeRotacao += 1;
+
+    requestAnimationFrame(loop);
+}
+
+function Start(){
+    PrepareCanvas();
+    PrepareShaders();
+    PrepareProgram();
+    PrepareTriangleData();
+    SendDataToShaders();
+
+    loop();
+}
